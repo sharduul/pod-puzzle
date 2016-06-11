1f5e9129a1a9b2fa06b3393b640a87ad2d665162
Shardul <bagadeshardul@gmail.com>
2016-06-11 11:09:25 -0400

    some commenting

--- space/spaceController.js
+++ space/spaceController.js
@@ -8,19 +8,23 @@
     SpaceController.$inject = ['spaceResource', '$sce'];
 
 	function SpaceController(spaceResource, $sce){
-		var vm = this;
+
+        // declare private variables here
+        var vm = this;
         var organizations = [];
 
+        // declare vm objects here
         vm.searchText = "";
         vm.filteredOrganizations = [];
 
+        // declare functions here
         vm.filter = filter;
         vm.highlight = highlight;
         vm.reset = reset;
 
-
 		(function(){
 
+            // get all the organizations and spaces
             spaceResource.space().get(function(data){
                 organizations = data;
                 vm.filteredOrganizations = data;
@@ -30,26 +34,38 @@
 		})();
 
 
+        // resets variables when search view is closed
         function reset(){
             vm.searchText = "";
             vm.filteredOrganizations = organizations;
         }
 
+
+        // function takes care of filtering logic
         function filter(){
 
             vm.searchText = vm.searchText.toLowerCase();
             vm.filteredOrganizations = [];
 
+
+            // if there is not search text
+            // show all the organizations and spaces
             if(!vm.searchText){
                 vm.filteredOrganizations = organizations;
                 return;
             }
 
+
             _.forEach(organizations, function(organizationItem){
 
+                // if search text matches any organization
+                // show all the spaces under it
                 if(organizationItem.name.toLowerCase().indexOf(vm.searchText) > -1){
                     vm.filteredOrganizations.push(organizationItem);
                 }
+
+                // if search does not match the organization name
+                // match individual spaces
                 else{
                     var tempOrg = _.clone(organizationItem);
                     tempOrg.spaces = [];
@@ -60,8 +76,10 @@
                             tempOrg.spaces.push(spaceItem);
                             match = true;
                         }
-                    })
+                    });
 
+                    // if any space is matched
+                    // show those spaces as well as the organization of it
                     if(match){
                         vm.filteredOrganizations.push(tempOrg);
                     }
@@ -71,13 +89,14 @@
         }
 
 
+        // takes care of highlighting the matched search text with results
+        // $sce service marks results as safe and can then be used by ng-bind-html directive
         function highlight(text, search) {
             if (!search) {
                 return $sce.trustAsHtml(text);
             }
             return $sce.trustAsHtml(text.replace(new RegExp(search, 'gi'), '<span class="highlightedText">$&</span>'));
-        };
-
+        }
 
 	}
 
