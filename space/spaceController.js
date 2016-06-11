ce4a1f79f86659a07b50f0841a9b5e9db186088c
Shardul <bagadeshardul@gmail.com>
2016-06-11 11:02:34 -0400

    filtering revamped

--- space/spaceController.js
+++ space/spaceController.js
@@ -5,23 +5,78 @@
 		.module('space-controllers', [])
 		.controller('spaceController', SpaceController);
 
-    SpaceController.$inject = ['spaceResource'];
+    SpaceController.$inject = ['spaceResource', '$sce'];
 
-	function SpaceController(spaceResource){
+	function SpaceController(spaceResource, $sce){
 		var vm = this;
+        var organizations = [];
 
-        vm.organizations = [];
+        vm.searchText = "";
+        vm.filteredOrganizations = [];
+
+        vm.filter = filter;
+        vm.highlight = highlight;
+        vm.reset = reset;
 
 
 		(function(){
 
             spaceResource.space().get(function(data){
-                vm.organizations = data;
+                organizations = data;
+                vm.filteredOrganizations = data;
                 console.log(data);
             });
 
 		})();
 
+
+        function reset(){
+            vm.searchText = "";
+            vm.filteredOrganizations = organizations;
+        }
+
+        function filter(){
+
+            vm.searchText = vm.searchText.toLowerCase();
+            vm.filteredOrganizations = [];
+
+            if(!vm.searchText){
+                vm.filteredOrganizations = organizations;
+                return;
+            }
+
+            _.forEach(organizations, function(organizationItem){
+
+                if(organizationItem.name.toLowerCase().indexOf(vm.searchText) > -1){
+                    vm.filteredOrganizations.push(organizationItem);
+                }
+                else{
+                    var tempOrg = _.clone(organizationItem);
+                    tempOrg.spaces = [];
+                    var match = false;
+
+                    _.forEach(organizationItem.spaces, function(spaceItem){
+                        if(spaceItem.name.toLowerCase().indexOf(vm.searchText) > -1){
+                            tempOrg.spaces.push(spaceItem);
+                            match = true;
+                        }
+                    })
+
+                    if(match){
+                        vm.filteredOrganizations.push(tempOrg);
+                    }
+                }
+
+            });
+        }
+
+
+        function highlight(text, search) {
+            if (!search) {
+                return $sce.trustAsHtml(text);
+            }
+            return $sce.trustAsHtml(text.replace(new RegExp(search, 'gi'), '<span class="highlightedText">$&</span>'));
+        };
 
 
 	}
