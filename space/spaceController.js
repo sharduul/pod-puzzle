be68afd0d652c740ad91010c8b4bbe1598fdb62c
Shardul <bagadeshardul@gmail.com>
2016-06-11 12:24:08 -0400

    keyboard navigation implemented... need some fixing

--- space/spaceController.js
+++ space/spaceController.js
@@ -5,17 +5,19 @@
 		.module('space-controllers', [])
 		.controller('spaceController', SpaceController);
 
-    SpaceController.$inject = ['spaceResource', '$sce'];
+    SpaceController.$inject = ['spaceResource', '$sce', '$document', '$timeout'];
 
-	function SpaceController(spaceResource, $sce){
+	function SpaceController(spaceResource, $sce, $document, $timeout){
 
         // declare private variables here
         var vm = this;
         var organizations = [];
+        var maxIndex = 0;
 
         // declare vm objects here
         vm.searchText = "";
         vm.filteredOrganizations = [];
+        vm.highlightIndex = 0;
 
         // declare functions here
         vm.filter = filter;
@@ -28,7 +30,8 @@
             spaceResource.space().get(function(data){
                 organizations = data;
                 vm.filteredOrganizations = data;
-                console.log(data);
+
+                indexifyResults();
             });
 
 		})();
@@ -42,7 +45,11 @@
 
 
         // function takes care of filtering logic
-        function filter(){
+        function filter(event){
+
+            if(event.keyCode == 40 || event.keyCode == 38){
+                return;
+            }
 
             vm.searchText = vm.searchText.toLowerCase();
             vm.filteredOrganizations = [];
@@ -52,40 +59,60 @@
             // show all the organizations and spaces
             if(!vm.searchText){
                 vm.filteredOrganizations = organizations;
-                return;
+            }
+            else{
+
+                _.forEach(organizations, function(organizationItem){
+
+                    // if search text matches any organization
+                    // show all the spaces under it
+                    if(organizationItem.name.toLowerCase().indexOf(vm.searchText) > -1){
+                        vm.filteredOrganizations.push(organizationItem);
+                    }
+
+                    // if search does not match the organization name
+                    // match individual spaces
+                    else{
+                        var tempOrg = _.clone(organizationItem);
+                        tempOrg.spaces = [];
+                        var match = false;
+
+                        _.forEach(organizationItem.spaces, function(spaceItem){
+                            if(spaceItem.name.toLowerCase().indexOf(vm.searchText) > -1){
+                                tempOrg.spaces.push(spaceItem);
+                                match = true;
+                            }
+                        });
+
+                        // if any space is matched
+                        // show those spaces as well as the organization of it
+                        if(match){
+                            vm.filteredOrganizations.push(tempOrg);
+                        }
+                    }
+
+                });
+
             }
 
 
-            _.forEach(organizations, function(organizationItem){
+            indexifyResults();
+            vm.highlightIndex = 0;
+        }
 
-                // if search text matches any organization
-                // show all the spaces under it
-                if(organizationItem.name.toLowerCase().indexOf(vm.searchText) > -1){
-                    vm.filteredOrganizations.push(organizationItem);
-                }
 
-                // if search does not match the organization name
-                // match individual spaces
-                else{
-                    var tempOrg = _.clone(organizationItem);
-                    tempOrg.spaces = [];
-                    var match = false;
+        function indexifyResults(){
 
-                    _.forEach(organizationItem.spaces, function(spaceItem){
-                        if(spaceItem.name.toLowerCase().indexOf(vm.searchText) > -1){
-                            tempOrg.spaces.push(spaceItem);
-                            match = true;
-                        }
-                    });
-
-                    // if any space is matched
-                    // show those spaces as well as the organization of it
-                    if(match){
-                        vm.filteredOrganizations.push(tempOrg);
-                    }
-                }
-
+            var index = 0;
+            _.forEach(vm.filteredOrganizations, function(organizationItem){
+                organizationItem.highlightIndex = index++;
+                _.forEach(organizationItem.spaces, function(spaceItem){
+                    spaceItem.highlightIndex = index++;
+                });
             });
+
+            maxIndex = index;
+
         }
 
 
@@ -97,6 +124,21 @@
             }
             return $sce.trustAsHtml(text.replace(new RegExp(search, 'gi'), '<span class="highlightedText">$&</span>'));
         }
+
+        $document.bind("keyup", function(event) {
+
+            $timeout(function(){
+                if(event.keyCode == 40){
+                    vm.highlightIndex = vm.highlightIndex + 1 <= maxIndex ? vm.highlightIndex + 1 : 0;
+                }
+                else if(event.keyCode == 38){
+                    vm.highlightIndex = vm.highlightIndex - 1 >= 0 ? vm.highlightIndex - 1 : maxIndex;
+                }
+            });
+
+        });
+
+
 
 	}
 
